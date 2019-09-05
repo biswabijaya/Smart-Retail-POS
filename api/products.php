38749fb56f3c69791d3a9d30418bb12f1fddbbe2
arunimamor <47169900+arunimamor@users.noreply.github.com>
2019-09-05 21:58:15 +0530

    Update products.php
    function for product sku

--- api/products.php
+++ api/products.php
@@ -4,4 +4,14 @@ include '../retail/shopdb.php';
 if (((isset($_GET['sku']) and isset($_GET['action'])) and $_GET['action']=="fetchproduct" )) {
   // code to find maching data from database
 }
+if (((isset($_GET['sku']) and isset($_GET['action'])) and $_GET['action']=="fetchproduct" )) {
+   echo getUserDataJson($_GET['sku']);
+}
+
+function getProductDAtaJson($sku=0){
+  if($query = mysqli_query($mysqli,"SELECT * FROM staffs WHERE sku = $sku")){
+    if ($row = mysqli_fetch_array($query)) {
+      json_encode($row);
+    }
+  }
 ?>
