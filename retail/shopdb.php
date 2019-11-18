d5957d5dfb0ac19d0fa408dff5cb5771c849104d
Biswabijaya Samal <me@biswabijaya.com>
2019-11-18 20:40:12 +0530

    Update shopdb.php

--- retail/shopdb.php
+++ retail/shopdb.php
@@ -12,9 +12,20 @@ $databasePassword = 'mko0<LP_';
 
 $mysqli =  mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
-function getMysqli()
-{
-  return mysqli_connect('localhost', 'u704461052_user', 'mko0<LP_', 'u704461052_shop');
+
+DEFINE ('DB_USER', 'u704461052_user');
+DEFINE ('DB_PASSWORD', 'mko0<LP_');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'u704461052_shop');
+
+function getMysqli() {
+  $dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+  if (!$dbc) {
+    trigger_error ('Could not connect to MySQL: ' . mysqli_connect_error() );
+  } else {
+    return $dbc;
+  }
 }
 
 $_SESSION['DIR']="";
