55e8996ff3563470f16e23c3a4f0ded7b48bbde8
Biswabijaya Samal <me@biswabijaya.com>
2019-11-18 20:08:39 +0530

    Update products.php

--- api/products.php
+++ api/products.php
@@ -2,13 +2,25 @@
 include '../retail/shopdb.php';
 
 if (((isset($_GET['sku']) and isset($_GET['action'])) and $_GET['action']=="fetchproduct" )) {
-   echo getProductDataJson($_GET['sku']);
+   echo getProduct$_GET['sku']);
 }
 
-function getProductDataJson($sku=0){
-  if($query = mysqli_query($mysqli,"SELECT * FROM staffs WHERE sku = $sku")){
+if ((isset($_GET['action'])) and $_GET['action']=="fetchproducts" ) {
+   echo getProducts();
+}
+
+function getProduct($sku=0){
+  if($query = mysqli_query($mysqli,"SELECT * FROM products WHERE sku = '$sku' ")){
     if ($row = mysqli_fetch_array($query)) {
       json_encode($row);
     }
   }
+
+function getProducts(){
+  if($query = mysqli_query($mysqli,"SELECT * FROM products order by category asc, subcategory asc, name asc ")){
+    if ($row = mysqli_fetch_array($query)) {
+      json_encode($row);
+    }
+  }
+
 ?>
