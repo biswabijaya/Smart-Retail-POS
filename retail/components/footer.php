b7a160d10bc5c4c3943d6b76fc1741edccb6e82a
Biswabijaya Samal <me@biswabijaya.com>
2019-12-06 20:59:37 +0530

    Update footer.php

--- retail/components/footer.php
+++ retail/components/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer">
   <div class="container">
     <div class="text-center">
-      <small> © 2019 Smart Retail POS | Made For <i class="fa fa-fw fa-heart" style="color:red;"></i> <a href="">Minor Project SRM University</a> By Aditya, Biswa, Arunima & Ishmin</small>
+      <small> © 2019 Smart Retail POS | Made By <i class="fa fa-fw fa-heart" style="color:red;"></i> <a href="wbweb.co.in">WB Web</a</small>
     </div>
   </div>
 </footer>
