69e4e172bc1839a8e19d853e24d7f045ae30465c
Biswabijaya Samal <me@biswabijaya.com>
2020-03-28 18:48:03 +0530

    Update getTable.php

--- api/getTable.php
+++ api/getTable.php
@@ -1,15 +1,24 @@
 <?php
 include 'db.php';
 
-function printTables($table='products'){
+function printTableSchema($table='products'){
   $json = array(); // declre array
-  if($result = mysqli_query(getMysqli(), "SELECT COLUMN_NAME
+  if($result = mysqli_query(getMysqli(), "SELECT *
   FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE TABLE_SCHEMA = 'u493086877_shop' AND TABLE_NAME = '$table';"))
+  WHERE TABLE_SCHEMA = 'u493086877_shop' AND TABLE_NAME = '$table'"))
     while($res = mysqli_fetch_assoc($result))
       $json[]=$res;
   echo json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 }
 
-printTables();
+function printTableData($table='products'){
+  $json = array(); // declre array
+  if($result = mysqli_query(getMysqli(), "SELECT * '$table'"))
+    while($res = mysqli_fetch_assoc($result))
+      $json[]=$res;
+  echo json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+}
+printTableSchema();
+echo "<hr>";
+printTableData();
 ?>
