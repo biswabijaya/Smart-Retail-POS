5e040749cc3293f11380c31e29d625603f2f4f10
Biswabijaya Samal <me@biswabijaya.com>
2020-03-28 19:02:37 +0530

    Update getTable.php

--- api/getTable.php
+++ api/getTable.php
@@ -2,11 +2,11 @@
 include 'db.php';
 
 function printTableSchema(){
-  $json = array(); // declre array
+  $js = array(); // declre array
   if($result = mysqli_query(getMysqli(), "SELECT @a:=@a+1 sr, ORDINAL_POSITION as tid , TABLE_NAME as tablename, COLUMN_NAME as columnname FROM INFORMATION_SCHEMA.COLUMNS,(SELECT @a:= 0) AS a WHERE TABLE_SCHEMA = 'u493086877_shop'"))
-    while($res = mysqli_fetch_assoc($result))
-      $json[]=$res;
-  echo json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    while($res = mysqli_fetch_array($result))
+      $js[]=$res;
+  echo json_encode($js, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 }
 
 function printTableData($table='products'){
