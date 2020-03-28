8b1f14ac411dddb07c8e2373f8eadf40b25d2db0
Biswabijaya Samal <me@biswabijaya.com>
2020-03-28 18:45:35 +0530

    Update and rename master.php to getTable.php

--- api/getTable.php
+++ api/getTable.php
@@ -1,9 +1,11 @@
 <?php
 include 'db.php';
 
-function printTables(){
+function printTables($table='products'){
   $json = array(); // declre array
-  if($result = mysqli_query(getMysqli(), "DESCRIBE my_table"))
+  if($result = mysqli_query(getMysqli(), "SELECT COLUMN_NAME
+  FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_SCHEMA = 'u493086877_shop' AND TABLE_NAME = '$table';"))
     while($res = mysqli_fetch_assoc($result))
       $json[]=$res;
   echo json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
