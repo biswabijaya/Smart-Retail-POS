096caa18ca1da0b0259904b13ab807c37fce5ef7
Biswabijaya Samal <me@biswabijaya.com>
2020-03-29 18:29:28 +0530

    Update priceTrend.php

--- charts/priceTrend.php
+++ charts/priceTrend.php
@@ -1,16 +1,29 @@
 <html>
     <head>
         <title>Item Price Trend</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
     </head>
     <body>
-        <div style="width:50%;">
-      		<canvas id="myChart" height="200"></canvas>
-      	</div>
-        <script src="http://smartretailpos.pe.hu/retail/assets/vendor/jquery/jquery.min.js"></script>
-        <script src="https://www.chartjs.org/dist/2.9.3/Chart.min.js"></script>
+      <div class="container">
+        <br><br>
+        <div class="row">
+          <div class="col-md-5">
+            <br><br><br><br>
+            <div id="selectdata">
 
+            </div>
+          </div>
+          <div class="col-md-7">
+            <canvas id="myChart" height="200"></canvas>
+          </div>
+        </div>
+      </div>
     </body>
-</html>
+    <script src="https://www.chartjs.org/dist/2.9.3/Chart.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
 <script>
 <?php
@@ -145,3 +158,4 @@ if (isset($_GET['sku'])) {
 
 
 </script>
+</html>
