03b9bd29e901bb7a9ff088fec7c1ca9f2120c058
Biswabijaya Samal <me@biswabijaya.com>
2020-03-29 18:16:30 +0530

    Update products.php

--- api/products.php
+++ api/products.php
@@ -1,6 +1,11 @@
 <?php
 include 'db.php';
 
+header("Access-Control-Allow-Origin: http://localhost:8000");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 if (!isset($_GET['sku'])) {
   echo '<form method="get">
   <select name="sku"><option>all</option>';
