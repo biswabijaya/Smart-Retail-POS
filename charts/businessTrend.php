00156e375cfbe9b510afddf8a2b268e744434e04
Biswabijaya Samal <me@biswabijaya.com>
2020-04-09 00:03:12 +0530

    Update businessTrend.php

--- charts/businessTrend.php
+++ charts/businessTrend.php
@@ -52,6 +52,47 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9/dist/sweetalert2.min.js"></script>
 
+  <script>
+  //SwalSetup
+  const Pop = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn-sm btn btn-success bg-success-dark-gradient',
+      cancelButton: 'btn-sm btn btn-danger bg-danger-dark-gradient',
+    },
+    showClass: {
+      popup: 'animated fadeInDown faster'
+    },
+    hideClass: {
+      popup: 'animated fadeOutUp faster'
+    },
+    buttonsStyling: true
+  });
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top',
+    showConfirmButton: false,
+    timer: 1000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  });
+  </script>
+
+  <script>
+  //ChatSetup
+  chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+  };
+  </script>
 <script>
 
 //businessTrendSetup
@@ -285,46 +326,5 @@ function changeBusunessTrendDate() {
 }
 </script>
 
-<script>
-//SwalSetup
-const Pop = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn-sm btn btn-success bg-success-dark-gradient',
-    cancelButton: 'btn-sm btn btn-danger bg-danger-dark-gradient',
-  },
-  showClass: {
-    popup: 'animated fadeInDown faster'
-  },
-  hideClass: {
-    popup: 'animated fadeOutUp faster'
-  },
-  buttonsStyling: true
-});
-
-const Toast = Swal.mixin({
-  toast: true,
-  position: 'top',
-  showConfirmButton: false,
-  timer: 1000,
-  timerProgressBar: true,
-  onOpen: (toast) => {
-    toast.addEventListener('mouseenter', Swal.stopTimer)
-    toast.addEventListener('mouseleave', Swal.resumeTimer)
-  }
-});
-</script>
-
-<script>
-//ChatSetup
-chartColors = {
-  red: 'rgb(255, 99, 132)',
-  orange: 'rgb(255, 159, 64)',
-  yellow: 'rgb(255, 205, 86)',
-  green: 'rgb(75, 192, 192)',
-  blue: 'rgb(54, 162, 235)',
-  purple: 'rgb(153, 102, 255)',
-  grey: 'rgb(201, 203, 207)'
-};
-</script>
 
 </html>
