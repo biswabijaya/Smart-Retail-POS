91ed29d2712d109380b9b2574e17d24c28c9e56a
aditysaachdeva98 <54707656+aditysaachdeva98@users.noreply.github.com>
2020-04-09 16:55:34 +0530

    Update auth.php

--- api/auth.php
+++ api/auth.php
@@ -3,7 +3,7 @@ include 'db.php';
 
 if (((isset($_GET['cno']) and isset($_GET['action'])) and $_GET['action']=="loginuser" )) {
   $cno = $_GET['cno']; $id = 0;
-  if($query = mysqli_query($mysqli,"SELECT * FROM staffs WHERE cno = $cno")){
+  if($query = mysqli_query(getMysqli(),"SELECT * FROM staffs WHERE cno = $cno")){
     while($row = mysqli_fetch_array($query)){
       $id = $row['id'];
     }
