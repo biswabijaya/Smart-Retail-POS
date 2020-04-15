5759a34158974568e102448aed21aa9c11655e5f
arunimamor <47169900+arunimamor@users.noreply.github.com>
2020-04-15 12:18:58 +0530

    Update getPurchases.php

--- api/getPurchases.php
+++ api/getPurchases.php
@@ -1,1 +1,19 @@
+<?php
+if(!isset($_GET['date'])){
+  echo '<form method="get">
+    <input type="date" name="date" value="">
+    <input type="submit" value="submit">
+  </form>';
+} else {
+printTableData($_GET['date']);
+}
 
+
+function printTableData($date){
+  $json = array(); // declre array
+  if($result = mysqli_query(getMysqli(), "SELECT * From purchases where date = '$date'"))
+    while($res = mysqli_fetch_assoc($result))
+      $json[]=$res;
+  echo json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+}
+?>
