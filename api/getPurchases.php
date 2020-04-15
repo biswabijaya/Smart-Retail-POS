27be5c62e52cbbfb6e2470c71498c10f95c35ad4
Biswabijaya Samal <me@biswabijaya.com>
2020-04-15 22:58:52 +0530

    add purchases api

--- api/getPurchases.php
+++ api/getPurchases.php
@@ -1,19 +1,25 @@
 <?php
-if(!isset($_GET['date'])){
+include 'db.php';
+
+if(!isset($_GET['fromdate']) or !isset($_GET['todate'])){
   echo '<form method="get">
-    <input type="date" name="date" value="">
+    <input type="date" name="fromdate" value="">
+    <input type="date" name="todate" value="">
     <input type="submit" value="submit">
   </form>';
 } else {
-printTableData($_GET['date']);
+printTableData($_GET['fromdate'],$_GET['todate']);
 }
 
 
-function printTableData($date){
+function printTableData($fromdate, $todate){
   $json = array(); // declre array
-  if($result = mysqli_query(getMysqli(), "SELECT * From purchases where date = '$date'"))
+  if($result = mysqli_query(getMysqli(), "SELECT * From purchases where date between '$fromdate' and '$todate'"))
     while($res = mysqli_fetch_assoc($result))
       $json[]=$res;
   echo json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 }
 ?>
+
+
+<!-- Adi: i need this and that, adi provide vars -->
